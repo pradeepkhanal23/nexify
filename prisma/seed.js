187e90563272808f44ac3b977b203d16ef80f2d8
Pradeep Khanal <pradeepkhanal642@gmail.com>
2025-01-19 20:25:37 +1100

    database seed file and function added to seed the database with the fake products

---
+++ prisma/seed.js
@@ -0,0 +1,25 @@
+// we need to make sure that we use the common js format here since its in the server
+
+const { PrismaClient } = require("@prisma/client");
+const products = require("./products.json");
+const prisma = new PrismaClient();
+
+// seeding the database with our fake products
+async function main() {
+  for (const product of products) {
+    await prisma.product.create({
+      data: product,
+    });
+  }
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+    console.log("Database seeded.........");
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    console.log("Database seeding failed.....");
+    process.exit(1);
+  });
