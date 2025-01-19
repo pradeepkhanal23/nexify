15bcea468f07466c5a53421f5f9ec40f01f8024a
Pradeep Khanal <pradeepkhanal642@gmail.com>
2025-01-19 20:17:58 +1100

    added a product model and some fake data just to work with the fetch and displaying of the data in the featured page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,3 @@
-import prisma from "@/utils/db";
-
 export default async function Home() {
-  const profile = await prisma.profile.create({
-    data: {
-      name: "Pradeep",
-    },
-  });
-
-  const data = await prisma.profile.findUnique({
-    where: {
-      id: profile.id,
-    },
-  });
-
-  return <>{data?.name}</>;
+  return <>Homepage</>;
 }
