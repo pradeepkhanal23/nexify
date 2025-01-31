a93ac24bb1058dbe56c3b12b1bec9045f90fcbb1
Pradeep Khanal <pradeepkhanal642@gmail.com>
2025-01-31 16:18:35 +1100

    toaster provider from shadcn added and tested

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,9 @@
 // our theme provider
 import ThemeProvider from "@/app/theme-provider";
 
+// out toast provider from shadcn
+import { Toaster } from "@/components/ui/toaster";
+
 // because we could have other globle providers like react query, authetication related stuff, we created a providers component that wraps the main layout and which ever provider we are using we can import here so that our app has access to all the provider from a same providers component
 // initially we start with theme provider, slowing building upto the toast provider and auth and many more
 // this provider component is very imp
@@ -13,6 +16,7 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
       enableSystem
       disableTransitionOnChange
     >
+      <Toaster />
       {children}
     </ThemeProvider>
   );
