32de3ab3c1396024fa9ff4faba4f865ccdb7d673
Pradeep Khanal <pradeepkhanal642@gmail.com>
2025-02-07 12:35:29 +1100

    added extra protection for the admin route in the middleware file

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,23 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 // we provide some public routes the users can access
 const isPublicRoute = createRouteMatcher(["/", "/products(.*)", "/about"]);
 
+// we want to restrict the admin access for the normal users
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
+
 // we setup the middleware in a way that if the req contains any other route than public , then its being protected by auth.protect() method
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) await auth.protect();
+
+  // check to see if the user is admin
+  const isAdminUser = (await auth()).userId === process.env.ADMIN_USER_ID;
+
+  // if the user is not an admin and tries to access the admin route, then we redirect the user to the homepage
+  if (!isAdminUser && isAdminRoute(req)) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
 });
 
 export const config = {
