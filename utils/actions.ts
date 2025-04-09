2082487dd027d0f527326a79e8f6bc343779b21e
Pradeep Khanal <pradeepkhanal642@gmail.com>
2025-04-10 09:28:25 +1000

    product creation tested with provided image, the create product action is working

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,5 +1,5 @@
 "use server";
-
+import { currentUser, auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import prisma from "./db";
 
@@ -13,14 +13,61 @@ export const fetchFeaturedProducts = async () => {
   return featuredProducts;
 };
 
+const renderError = (
+  error: unknown
+): {
+  message: string;
+} => {
+  console.log(error);
+
+  return {
+    message: error instanceof Error ? error.message : "An error occurred",
+  };
+};
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    throw new Error("You must be logged in to access this route!!");
+  }
+
+  return user;
+};
+
 // creating a product action
 export const createProductAction = async (
   prevState: any,
   formData: FormData
 ): Promise<{ message: string }> => {
-  return {
-    message: "Product Created",
-  };
+  const user = await getAuthUser();
+
+  try {
+    const name = formData.get("name") as string;
+    const company = formData.get("company") as string;
+    // the price is a string to begin with so using Number() constructor we turned it into a number
+    const price = Number(formData.get("price") as string);
+    const image = formData.get("image") as File;
+    const description = formData.get("description") as string;
+
+    // converting featured into boolean as well
+    const featured = Boolean(formData.get("featured") as string);
+
+    await prisma.product.create({
+      data: {
+        name,
+        company,
+        price,
+        image: "/images/apple-watch.png",
+        description,
+        featured,
+        clerkId: user.id,
+      },
+    });
+    return { message: "product created" };
+  } catch (error) {
+    return renderError(error);
+  }
 };
 
 // fetching all the products OR fetch based on the search provided
